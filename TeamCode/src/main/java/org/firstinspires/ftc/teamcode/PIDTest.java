01025c49a4603cef68f1ea178155fae96d967806
23pavel <23pavel@lakeridgeacademy.org>
2023-03-09 17:35:16 -0500

    Formatted Jack's code though I didn't test it yet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDTest.java
@@ -6,46 +6,35 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name = "MecanumDrive", group = "test")
+@TeleOp(name = "MecanumDrive", group = "PIDTest")
 
-public class Test extends LinearOpMode {
+public class PIDTest extends LinearOpMode {
     private final ElapsedTime runtime = new ElapsedTime();
-
-
-    private DcMotor epicMotor;
 
     @SuppressLint("DefaultLocale")
     @Override
     public void runOpMode() {
-        telemetry.addData("Status", "do the stuff");
-        telemetry.speak("this will not at all become extremely annoying in the future");
-
+        telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        waitForStart();
-        runtime.reset();
+        DcMotor motor1 = hardwareMap.get(DcMotor.class, "motor 1");
 
-
-        epicMotor = hardwareMap.get(DcMotor.class,"motor 1");
-        double position = epicMotor.getCurrentPosition()*(1f/28f)*(360f/1f)*(1f/20f);
-
+        double position = motor1.getCurrentPosition()*(1f/28f)*(360f/1f)*(1f/20f);
         double target = 0;
-
         double Kp = 0.04;
         double Ki = 0.00001;
         double Kd = 0.001;
-
-
-
         double time = getRuntime();
         double previousTime = time;
-
         double error = target - position;
         double previousError = error;
         double errorSum = 0;
 
+        waitForStart();
+        runtime.reset();
+
         while(opModeIsActive()) {
-            position = epicMotor.getCurrentPosition()*(1f/28f)*(360f/1f)*(1f/20f);
+            position = motor1.getCurrentPosition()*(1f/28f)*(360f/1f)*(1f/20f);
             time = getRuntime();
             error = target - position;
             errorSum += error;
@@ -57,29 +46,23 @@ public class Test extends LinearOpMode {
             double I = Ki*errorSum;
             double D = (changeInError/frameTime)*Kd;
 
-            epicMotor.setPower(P+I+D);
+            motor1.setPower(P+I+D);
 
             target += gamepad1.right_stick_x*frameTime*2000;
 
             previousTime = time;
             previousError = error;
 
-
-
-
-
-            telemetry.addData("runtime",getRuntime());
-            telemetry.addData("frameTime",frameTime);
-            telemetry.addData("target",target);
-            telemetry.addData("current position",position);
-            telemetry.addData("error",error);
-            telemetry.addData("change in error",changeInError);
-            telemetry.addData("P ",P);
-            telemetry.addData("I " ,I);
-            telemetry.addData("D ",D);
-
-            telemetry.addData("motor power ",epicMotor.getPower()*100d);
-
+            telemetry.addData("Runtime", getRuntime());
+            telemetry.addData("Frame Time", frameTime);
+            telemetry.addData("Target", target);
+            telemetry.addData("Current Position", position);
+            telemetry.addData("Error", error);
+            telemetry.addData("Change in Error", changeInError);
+            telemetry.addData("P", P);
+            telemetry.addData("I", I);
+            telemetry.addData("D", D);
+            telemetry.addData("Motor Power", motor1.getPower()*100d);
             telemetry.update();
         }
     }
